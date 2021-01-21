683240b29143cb83958356f87e86d09f6c69f1e0
9831065 <najafizadeh21@gmail.com>
2021-01-21 11:35:12 +0330

    Downloader build

---
+++ src/link/Downloader.java
@@ -0,0 +1,34 @@
+package link;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.time.Duration;
+
+public class Downloader
+{
+    public boolean touchLink(String link)
+    {
+        File output = new File("result.zip");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(link))
+                .timeout(Duration.ofMinutes(1))
+                .build();
+        try
+        {
+            HttpResponse<Path> response = client.send(request, HttpResponse.BodyHandlers.ofFile(output.toPath()));
+            System.out.println(response.statusCode() + " " + response.version());
+            System.out.println(response.headers());
+            return true;
+        } catch (IOException | InterruptedException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
--- src/link/Main.java
+++ src/link/Main.java
@@ -1,30 +1,27 @@
 package link;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
+import java.util.Scanner;
 
 public class Main
 {
+    public static void messageInConsole(String message)
+    {
+        System.out.print(message);
+    }
+
     public static void main(String[] args)
     {
-        File output = new File("result.html");
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.google.com"))
-                .timeout(Duration.ofMinutes(1))
-                .build();
-        try
+        Downloader downloader = new Downloader();
+        Scanner scanner = new Scanner(System.in);
+        messageInConsole("Enter url > ");
+        String link = scanner.next();
+        boolean result = downloader.touchLink(link);
+        if (result)
         {
-            HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofFile(output.toPath()));
-            System.out.println(response.statusCode() + " " + response.version());
-        } catch (IOException | InterruptedException e)
+            messageInConsole("OK");
+        } else
         {
-            e.printStackTrace();
+            messageInConsole("FAILED");
         }
     }
 }
