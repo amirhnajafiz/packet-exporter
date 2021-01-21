f03eb3b68cc7e3460de18e3d15865dad8fcbecdc
9831065 <najafizadeh21@gmail.com>
2021-01-21 11:16:13 +0330

    Http client first set

--- src/link/Main.java
+++ src/link/Main.java
@@ -1,8 +1,28 @@
 package link;
 
-public class Main {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+    public static void main(String[] args)
+    {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.google.com"))
+                .timeout(Duration.ofMinutes(1))
+                .build();
+        try
+        {
+            HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.statusCode() + " " + response.version());
+        } catch (IOException | InterruptedException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
