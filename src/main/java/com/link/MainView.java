1dcedbf476f40f4b525724112dd1c0fed8cf1285
9831065 <najafizadeh21@gmail.com>
2021-03-16 18:28:44 +0330

    Problem with module of index.html

--- src/main/java/com/link/MainView.java
+++ src/main/java/com/link/MainView.java
@@ -4,7 +4,8 @@ import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
 
-@JavaScript("frontend://script.js")
+
+@JavaScript("./frontend://script.js")
 @Route
 public class MainView extends Div {
     public MainView() {
