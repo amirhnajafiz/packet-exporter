809061d6ee8453494150c160741cefaa4b0873d0
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-20 14:55:25 +0330

    add: google connection

--- ping-google/script.py
+++ ping-google/script.py
@@ -3,6 +3,13 @@ import sys
 
 
 
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print ("socket successfully created")
+except socket.error as err:
+    print ("socket creation failed with error %s" %(err))
+    sys.exit()
+
 # let's open a connection to google with socket
 PORT = 80
 
@@ -12,3 +19,7 @@ try:
 except socket.gaierror as err:
     print("cannot find google ip %s" % (err))
     sys.exit()
+
+s.connect((host, PORT))
+
+print ("the socket has successfully connected to google")
