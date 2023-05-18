e6fcb04f7cc32a93bb17d04a0a01ec1b90d4736e
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-18 10:21:49 +0330

    fix: styles

--- src/App.vue
+++ src/App.vue
@@ -23,13 +23,19 @@ import { RouterLink, RouterView } from 'vue-router'
 }
 
 nav {
+  display: flex;
+  align-content: center;
+  font-size: 0.8em;
   inline-size: 100%;
-  margin: 20px 0;
-  padding: 15px 100px;
+  padding: 30px 100px;
+  background: rgb(255, 109, 109);
 }
 
 .link {
-  color: rgb(80, 80, 80);
+  background: #ffffff;
+  padding: 7px;
+  border-radius: 5px;
+  color: rgb(30, 30, 30);
   outline: none;
   text-decoration: none;
   margin-inline-end: 50px;
@@ -40,6 +46,7 @@ nav {
 }
 
 .right-link {
-  float: right;
+  margin-inline-start: auto;
+  order: 2;
 }
 </style>
