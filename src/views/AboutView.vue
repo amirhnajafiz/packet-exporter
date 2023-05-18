4ebcd9d013c1ca8e1f1f102d7a8efbd0d46e8ac3
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-18 10:02:17 +0330

    update: about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>
+      About Stallion
+    </h1>
+    <p>
+      Stallion is a Message Broker that is implemented with Go programming language.
+      You can use Stallion to publish events on specific topics and consume them over client side with SDKs.
+      Stallion provides authentication so you can authorise your clients.
+      One good thing about Stallion is that it uses only Golang internal modules and has no external modules.
+      Everything in Stallion is pure Golang.
+      It comes with Load Test (Race) and Blackbox Exporter (Vet) to analyse the Stallion server.
+    </p>
   </div>
 </template>
