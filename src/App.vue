560be2ccffd79bce464625f3f3bf0b2091e33d91
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-18 10:12:23 +0330

    add: navbar

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,31 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="wrapper">
+    <div>
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink class="link" to="/">Home</RouterLink>
+        <RouterLink class="link" to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
   <RouterView />
 </template>
+
+<style scoped>
+nav {
+  inline-size: 100%;
+  margin: 20px 0;
+  padding: 0 20px;
+}
+
+.link {
+  color: rgb(80, 80, 80);
+  outline: none;
+  text-decoration: none;
+  margin-inline-end: 30px;
+}
+
+.link:hover {
+  color: rgb(120, 120, 120);
+}
+</style>
