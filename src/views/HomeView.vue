6cc927fe7cd74e19cbc8555fbb73938b39e4af33
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-18 12:13:15 +0330

    add: main logo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    Welcome to Stallion official website.
-  </div>
+    <img class="img" src="../assets/stallion.png" alt="main-logo" />
 </template>
+
+<style scoped>
+.img {
+  display: block;
+  margin: 100px auto;
+  inline-size: 40%;
+  box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.2);
+}
+</style>
