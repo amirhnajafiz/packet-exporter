615d52dbd91bda8a590eefee04ed21747ac23bd2
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-18 12:40:33 +0330

    feat: nav links

--- src/views/Documents.vue
+++ src/views/Documents.vue
@@ -3,7 +3,10 @@
     Comming Soon!
   </h1>
   <p class="about-text">
-    Right now you can find all of the documents on Stallion organization in Github.
+    Right now you can find all of the documents on Stallion organization in Github.<br />
+    Check the organization in <a href="https://github.com/official-stallion">here</a>.<br /><br />
+    Full documents will be available on Augest 2023.<br />
+    Thanks for supporting us.
   </p>
 </template>
 
