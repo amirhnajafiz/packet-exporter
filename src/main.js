36d01d17bde7c207e71b4a4e4f4281cc9d01ba5b
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-18 10:07:44 +0330

    fix: bug of run

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(BootstrapVue)
-app.use(IconsPlugin)
 
 app.mount('#app')
