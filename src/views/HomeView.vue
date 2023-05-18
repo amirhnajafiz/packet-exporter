d2794b5358b8c004b2830f65d3de6686e70a19bf
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-18 12:18:46 +0330

    add: main text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,22 @@
 <template>
     <img class="img" src="../assets/stallion.png" alt="main-logo" />
+    <small class="ref">
+      Fast and Reliable Message Broker
+    </small>
 </template>
 
 <style scoped>
 .img {
   display: block;
-  margin: 100px auto;
+  margin: 100px auto 0;
   inline-size: 40%;
   box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.2);
+}
+
+.ref {
+  display: block;
+  margin-block-start: 30px;
+  text-align: center;
+  color: rgb(120, 120, 120);
 }
 </style>
