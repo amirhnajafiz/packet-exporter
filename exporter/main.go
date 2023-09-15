2b86f77b85723145799d5d0b37cf54534bc9d363
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-15 19:26:17 +0330

    add: exporter structure

--- exporter/main.go
+++ exporter/main.go
@@ -6,6 +6,18 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+type response struct {
+	Requests     int       `json:"requests"`
+	Logs         int       `json:"logs"`
+	ResponseTime []float64 `json:"response_time"`
+}
+
+func pull(port, interval int) {
+	// register metrics
+	// make http request to metrics endpoint
+	// update metrics
+}
+
 func main() {
 	http.Handle("/metrics", promhttp.Handler())
 
