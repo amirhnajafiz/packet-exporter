697ee578be7037a2fefbb739c566678325726cc0
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-15 11:30:04 +0330

    add: index bug

--- app/index.js
+++ app/index.js
@@ -8,21 +8,49 @@ const logFile = fs.createWriteStream('log.txt', { flags: 'a' });
 const logStdout = process.stdout;
 
 
+// create metrics
+let metrics = {
+  "requests": 0,
+  "logs": 0,
+  "response_time": []
+}
+
+
+// export log into error
 console.error = console.log;
 
 
 app.get('/', (req, res) => {
-    console.log(`${new Date()}[${req.method}] path: ${req.url}`);
-    res.send('Hello World!');
+  let startTime = new Date();
+
+  console.log(`${new Date()}[${req.method}] path: ${req.url}`);
+
+  metrics.requests++;
+
+  let endTime = new Date();
+
+  metrics.response_time.push(endTime - startTime);
+
+  res.send('Hello World!');
 });
 
 app.post('/log', (req, res) => {
-    console.log(`${new Date()}[${req.method}] path: ${req.url}`);
-    res.send('Logged!');
+  let startTime = new Date();
+
+  console.log(`${new Date()}[${req.method}] path: ${req.url}`);
+
+  metrics.requests++;
+  metrics.logs++;
+
+  let endTime = new Date();
+
+  metrics.response_time.push(endTime - startTime);
+
+  res.send('Logged!');
 });
 
 app.listen(port, () => {
-  console.log(`example app listening on port ${port}`);
+  console.log(`app is listening on port ${port} ...`);
 
   console.log = function () {
     logFile.write(util.format.apply(null, arguments) + '\n');
