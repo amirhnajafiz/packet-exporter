50cb8ec2d3ad5e7d6914f6add3c26125c04cb790
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-15 19:22:59 +0330

    add: promhttp

--- exporter/main.go
+++ exporter/main.go
@@ -1,3 +1,15 @@
 package main
 
-func
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func main() {
+	http.Handle("/metrics", promhttp.Handler())
+
+	if err := http.ListenAndServe(":2112", nil); err != nil {
+		panic(err)
+	}
+}
