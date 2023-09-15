9ead218b16890c29fa81356994a5288a92b99278
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-15 19:29:27 +0330

    add: options

--- exporter/main.go
+++ exporter/main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-type response struct {
-	Requests     int       `json:"requests"`
-	Logs         int       `json:"logs"`
-	ResponseTime []float64 `json:"response_time"`
-}
+type (
+	response struct {
+		Requests     int       `json:"requests"`
+		Logs         int       `json:"logs"`
+		ResponseTime []float64 `json:"response_time"`
+	}
 
-func pull(port, interval int) {
+	metrics struct {
+		Requests *prometheus.Counter
+		Logs     *prometheus.Counter
+		Response prometheus.Histogram
+	}
+)
+
+func pull(port, interval int) error {
 	// register metrics
+
 	// make http request to metrics endpoint
+	request, err := http.NewRequest(http.MethodGet, fmt.Sprintf("127.0.0.1:%d", port), nil)
+	if err != nil {
+		return err
+	}
+
 	// update metrics
 }
 
