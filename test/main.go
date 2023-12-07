7b801e178764d859b4ce3a29238ba464e7d23a7b
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-07 11:33:28 +0330

    add: main test script

--- test/main.go
+++ test/main.go
@@ -1,1 +1,43 @@
-package test
+package main
+
+import (
+	"errors"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func main() {
+	var lvl zapcore.Level
+	if err := lvl.Set(os.Getenv("LEVEL")); err != nil {
+		log.Printf("cannot parse log level %s: %s", os.Getenv("LEVEL"), err)
+
+		lvl = zapcore.WarnLevel
+	}
+
+	encoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+	defaultCore := zapcore.NewCore(encoder, zapcore.Lock(zapcore.AddSync(os.Stderr)), lvl)
+	cores := []zapcore.Core{
+		defaultCore,
+	}
+
+	core := zapcore.NewTee(cores...)
+	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
+
+	{
+		logger.Debug("debug massage", zap.Time("now", time.Now()))
+	}
+	{
+		logger.Warn("warn massage", zap.Int("data", rand.Int()), zap.Int("key", rand.Int()))
+	}
+	{
+		logger.Info("info massage", zap.String("id", "9902777462"))
+	}
+	{
+		logger.Error("error massage", zap.Error(errors.New("failed to load")))
+	}
+}
