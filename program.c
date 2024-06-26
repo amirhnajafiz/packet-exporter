3a13281a55c2d0779888fe917d007d717098391b
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-26 20:35:10 +0330

    add: program.c

---
+++ program.c
@@ -0,0 +1,66 @@
+#include <uapi/linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+struct packet_info {
+    __u32 src_ip;
+    __u32 dest_ip;
+    __u16 src_port;
+    __u16 dest_port;
+    __u8 protocol;
+    __u32 ifindex;
+    __u32 payload_len;
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+    __uint(max_entries, 1024);
+} events SEC(".maps");
+
+SEC("xdp")
+int packet_monitor(struct xdp_md *ctx) {
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    struct ethhdr *eth = data;
+
+    if ((void *)(eth + 1) > data_end) {
+        return XDP_DROP;
+    }
+
+    struct iphdr *ip = data + sizeof(*eth);
+    if ((void *)(ip + 1) > data_end) {
+        return XDP_DROP;
+    }
+
+    struct packet_info pkt = {};
+    pkt.src_ip = ip->saddr;
+    pkt.dest_ip = ip->daddr;
+    pkt.protocol = ip->protocol;
+    pkt.ifindex = ctx->ingress_ifindex;
+    pkt.payload_len = data_end - data;
+
+    if (ip->protocol == IPPROTO_TCP) {
+        struct tcphdr *tcp = (void *)ip + sizeof(*ip);
+        if ((void *)(tcp + 1) > data_end) {
+            return XDP_DROP;
+        }
+        pkt.src_port = tcp->source;
+        pkt.dest_port = tcp->dest;
+    } else if (ip->protocol == IPPROTO_UDP) {
+        struct udphdr *udp = (void *)ip + sizeof(*ip);
+        if ((void *)(udp + 1) > data_end) {
+            return XDP_DROP;
+        }
+        pkt.src_port = udp->source;
+        pkt.dest_port = udp->dest;
+    }
+
+    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &pkt, sizeof(pkt));
+    return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
