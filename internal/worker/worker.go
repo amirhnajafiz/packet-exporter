0588fbe098743895bd511278fdcbc413f3731462
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-26 21:40:33 +0330

    fix: workers

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -17,26 +17,28 @@ type worker struct {
 }
 
 func (w worker) work() {
-	// listen for packet events
+	// listen for packetmeta events over the give channel by XDP reader
 	for pkt := range w.channel {
+		// find interface
 		link, err := netlink.LinkByIndex(int(pkt.IfIndex))
 		if err != nil {
 			log.Printf("failed to get interface name: %v", err)
 			continue
 		}
 
+		// export data
 		ifaceName := link.Attrs().Name
 		srcIP := fmt.Sprintf("%d.%d.%d.%d", pkt.SrcIP&0xff, (pkt.SrcIP>>8)&0xff, (pkt.SrcIP>>16)&0xff, (pkt.SrcIP>>24)&0xff)
 		destIP := fmt.Sprintf("%d.%d.%d.%d", pkt.DestIP&0xff, (pkt.DestIP>>8)&0xff, (pkt.DestIP>>16)&0xff, (pkt.DestIP>>24)&0xff)
+		src := fmt.Sprintf("%s:%d", srcIP, pkt.SrcPort)
+		dest := fmt.Sprintf("%s:%d", destIP, pkt.DestPort)
 
 		log.Printf(
 			"Packet: Interface=%s, SrcIP=%s, DestIP=%s, SrcPort=%d, DestPort=%d, Protocol=%d, PayloadLen=%d",
 			ifaceName, srcIP, destIP, pkt.SrcPort, pkt.DestPort, pkt.Protocol, pkt.PayloadLen,
 		)
 
-		src := fmt.Sprintf("%s:%d", srcIP, pkt.SrcPort)
-		dest := fmt.Sprintf("%s:%d", destIP, pkt.DestPort)
-
+		// export metrics
 		w.metrics.IncRequest(src, dest, ifaceName, int(pkt.Protocol))
 		w.metrics.ObserveThroughput(src, dest, ifaceName, int(pkt.Protocol), float64(pkt.PayloadLen))
 	}
