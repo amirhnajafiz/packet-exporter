2d5e1f446a5a44f6367233aa0437acaffd851027
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-27 11:05:03 +0330

    remove: dup code from reader

--- internal/xdp/reader.go
+++ internal/xdp/reader.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/amirhnajafiz/packet-exporter/internal/model"
 	"github.com/cilium/ebpf/perf"
-	"github.com/vishvananda/netlink"
 )
 
 // Reader sets up a perf read to read packet events and publish them
@@ -53,20 +52,6 @@ func (x *XDPManager) Reader() (chan *model.PacketMeta, error) {
 
 			// publish over this channel
 			channel <- &pkt
-
-			// get link by its index
-			link, err := netlink.LinkByIndex(int(pkt.IfIndex))
-			if err != nil {
-				log.Printf("failed to get interface name: %v", err)
-				continue
-			}
-
-			ifaceName := link.Attrs().Name
-			srcIP := fmt.Sprintf("%d.%d.%d.%d", pkt.SrcIP&0xff, (pkt.SrcIP>>8)&0xff, (pkt.SrcIP>>16)&0xff, (pkt.SrcIP>>24)&0xff)
-			destIP := fmt.Sprintf("%d.%d.%d.%d", pkt.DestIP&0xff, (pkt.DestIP>>8)&0xff, (pkt.DestIP>>16)&0xff, (pkt.DestIP>>24)&0xff)
-
-			log.Printf("Packet: Interface=%s, SrcIP=%s, DestIP=%s, SrcPort=%d, DestPort=%d, Protocol=%d, PayloadLen=%d",
-				ifaceName, srcIP, destIP, pkt.SrcPort, pkt.DestPort, pkt.Protocol, pkt.PayloadLen)
 		}
 	}()
 
