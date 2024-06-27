0af1cb8fe16063bd7bd5560ba9c1505cf6259148
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-27 11:13:13 +0330

    add: metrics in promauto

--- internal/monitoring/metrics/metrics.go
+++ internal/monitoring/metrics/metrics.go
@@ -1,11 +1,35 @@
 package metrics
 
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
 // Metrics sotres exporter prometheus metrics.
-type Metrics struct{}
+type Metrics struct {
+	requests   *prometheus.CounterVec
+	throughput *prometheus.HistogramVec
+}
 
 // New returns a new metrics instance.
 func New() *Metrics {
-	return &Metrics{}
+	labels := []string{
+		"source",
+		"dest",
+		"interface",
+		"protocol",
+	}
+
+	return &Metrics{
+		requests: promauto.NewCounterVec(prometheus.CounterOpts{
+			Name: "total_packets",
+			Help: "counting total packets of an interface",
+		}, labels),
+		throughput: promauto.NewHistogramVec(prometheus.HistogramOpts{
+			Name: "total_throughput",
+			Help: "observe the total payload that is being transfer through an interface",
+		}, labels),
+	}
 }
 
 // IncRequest based on its source, dest, protocol, and ifname.
